a2aa365c88609138fb8f1153bb814109ac43eb57
shellbear <antoine.ordonez@epitech.eu>
2019-06-11 11:17:52 +0200

    Add serve method

--- server.js
+++ server.js
@@ -9,4 +9,6 @@ app.get('/', function(req, res) {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
-app.listen(9000);
+app.listen(9000, "127.0.0.1", () =>
+  console.debug('Listing on http://127.0.0.1:9000/')
+);
